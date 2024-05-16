76ab1fc9b7f4e100abe1297b5290e6f49f92bab7
Breadus1 <marcel.0505@wp.pl>
2024-05-16 13:09:48 +0200

    działa

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CompanyPanel from '../pages/CompanyPanel';
-import VotingPanel from '../pages/VotingPanel';
-import Login from '../pages/Login';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CompanyPanel from "../pages/CompanyPanel";
+import VotingPanel from "../pages/VotingPanel";
+import Login from "../pages/Login";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/company-panel" element={<CompanyPanel />} />
         <Route path="/voting-panel" element={<VotingPanel />} />
-        <Route path="/" exact element={<Login />} />
+        <Route path="/" exact element={<VotingPanel />} />
       </Routes>
       <Footer />
     </Router>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
-      <h1>Welcome to VoteStar</h1>
+      <h1>Welcome to VoteForStar</h1>
+      <nav>
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/company-panel">
+          <button>Register company</button>
+        </Link>
+      </nav>
     </header>
   );
 }
